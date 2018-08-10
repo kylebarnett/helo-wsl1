a26c8ab5b6d8013c3d4e62b3dc1c67645d492e1f
Kyle Barnett <kbarnett23@gmail.com>
2018-08-10 09:35:41 -0600

    setup react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Auth from './components/Auth';
+import Dashboard from './components/Dashboard';
+import Form from './components/Form';
+import Post from './components/Post';
+import Nav from './components/Nav'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Nav />
+        <Auth />
+        <Dashboard />
+        <Form />
+        <Post />
       </div>
     );
   }
